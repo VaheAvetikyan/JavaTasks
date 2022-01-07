76005123c51cc7e4e9eede1d93fc1bf91bcb5929
VaheAvetikyan <vahe.avetikyan@spiritofsoft.com>
2022-01-07 13:39:09 +0400

    Add the best solution for FindPairs

--- src/competitive/hackerrank/FindPairs.java
+++ src/competitive/hackerrank/FindPairs.java
@@ -5,48 +5,115 @@ import java.util.*;
 public class FindPairs {
     public static void main(String[] args) {
         final int EXECUTION_TIMES = 100;
-        List<Integer> numbers12 = List.of(1, 2);
-        List<Integer> numbers = List.of(1, 1, 2, 2, 3, 3);
-        List<Integer> nums = List.of(1, 2, 3, 4, 5, 6);
-        List<Integer> nus = List.of(1, 2, 5, 6, 9, 10);
+        List<Integer> numbers12 = new ArrayList<>(Arrays.asList(1, 2));
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 1, 2, 2, 3, 3));
+        List<Integer> nums = new ArrayList<>(Arrays.asList(6, 4, 1, 3, 5, 2));
+        List<Integer> nus = new ArrayList<>(Arrays.asList(1, 2, 5, 6, 9, 10));
+        List<Integer> numbersBig = new ArrayList<>();
+        List<Integer> numbersBigShuffled = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            numbersBig.add(i);
+            numbersBigShuffled.add(i);
+        }
+        Collections.shuffle(numbersBigShuffled);
 
         long time = System.currentTimeMillis();
         for (int i = 0; i < EXECUTION_TIMES; i++) {
-            System.out.println(PairUtil.countPairs(numbers12, 1));
+            System.out.println("Count of pairs: " + PairUtil.countPairs(numbers12, 1));
             System.out.println();
 
-            System.out.println(PairUtil.countPairs(numbers, 1));
+            System.out.println("Count of pairs: " + PairUtil.countPairs(numbers, 1));
             System.out.println();
 
-            System.out.println(PairUtil.countPairs(nums, 2));
+            System.out.println("Count of pairs: " + PairUtil.countPairs(nums, 2));
             System.out.println();
 
-            System.out.println(PairUtil.countPairs(nus, 2));
+            System.out.println("Count of pairs: " + PairUtil.countPairs(nus, 2));
+            System.out.println();
+
+            System.out.println("Count of pairs: " + PairUtil.countPairs(numbersBig, 2));
+            System.out.println();
+
+            System.out.println("Count of pairs: " + PairUtil.countPairs(numbersBigShuffled, 2));
             System.out.println();
         }
-        long firstTime = System.currentTimeMillis() - time;
+        long countPairsTime = System.currentTimeMillis() - time;
 
         time = System.currentTimeMillis();
         for (int i = 0; i < EXECUTION_TIMES; i++) {
-            System.out.println(PairUtil.countPairsBrutForce(numbers12, 1));
+            System.out.println("Count of pairs: " + PairUtil.countPairsOther(numbers12, 1));
             System.out.println();
 
-            System.out.println(PairUtil.countPairsBrutForce(numbers, 1));
+            System.out.println("Count of pairs: " + PairUtil.countPairsOther(numbers, 1));
             System.out.println();
 
-            System.out.println(PairUtil.countPairsBrutForce(nums, 2));
+            System.out.println("Count of pairs: " + PairUtil.countPairsOther(nums, 2));
             System.out.println();
 
-            System.out.println(PairUtil.countPairsBrutForce(nus, 2));
+            System.out.println("Count of pairs: " + PairUtil.countPairsOther(nus, 2));
+            System.out.println();
+
+            System.out.println("Count of pairs: " + PairUtil.countPairsOther(numbersBig, 2));
+            System.out.println();
+
+            System.out.println("Count of pairs: " + PairUtil.countPairsOther(numbersBigShuffled, 2));
             System.out.println();
         }
-        System.out.println("execution of countPairs() took " + firstTime + "ms");
+        long countPairsOtherTime = System.currentTimeMillis() - time;
+
+        time = System.currentTimeMillis();
+        for (int i = 0; i < EXECUTION_TIMES; i++) {
+            System.out.println("Count of pairs: " + PairUtil.countPairsBrutForce(numbers12, 1));
+            System.out.println();
+
+            System.out.println("Count of pairs: " + PairUtil.countPairsBrutForce(numbers, 1));
+            System.out.println();
+
+            System.out.println("Count of pairs: " + PairUtil.countPairsBrutForce(nums, 2));
+            System.out.println();
+
+            System.out.println("Count of pairs: " + PairUtil.countPairsBrutForce(nus, 2));
+            System.out.println();
+
+            System.out.println("Count of pairs: " + PairUtil.countPairsBrutForce(numbersBig, 2));
+            System.out.println();
+
+            System.out.println("Count of pairs: " + PairUtil.countPairsBrutForce(numbersBigShuffled, 2));
+            System.out.println();
+        }
+
+        System.out.println("execution of countPairs() took " + countPairsTime + "ms");
+        System.out.println("execution of countPairsOther() took " + countPairsOtherTime + "ms");
         System.out.println("execution of countPairsBrutForce() took " + (System.currentTimeMillis() - time) + "ms");
     }
 }
 
 class PairUtil {
     public static int countPairs(List<Integer> numbers, int k) {
+        Collections.sort(numbers);
+        Set<Pair> pairs = new HashSet<>();
+        int iterationCount = 0;
+        for (int i = 0, j = 0; i < numbers.size() && j < numbers.size(); ) {
+            iterationCount++;
+
+            int a = numbers.get(i);
+            int b = numbers.get(j);
+            if (a + k == b) {
+                pairs.add(new Pair(a, b));
+                i++;
+                j++;
+            } else if (a + k < b) {
+                i++;
+            } else if (a + k > b) {
+                j++;
+            }
+        }
+
+        System.out.println("countPairs() iteration count: " + iterationCount);
+        return pairs.size();
+    }
+
+    public static int countPairsOther(List<Integer> numbers, int k) {
         Set<Pair> pairs = new HashSet<>();
         int iterationCount = 0;
         for (int i = 0; i < numbers.size(); i++) {
@@ -56,13 +123,11 @@ class PairUtil {
                 int a = numbers.get(i);
                 int b = numbers.get(j);
                 if (a + k == b) {
-                    boolean added = pairs.add(new Pair(a, b));
-                    if (added) System.out.println(a + ", " + b + " pair added");
+                    pairs.add(new Pair(a, b));
                 }
             }
         }
-
-        System.out.println("countPairs() iteration count: " + iterationCount);
+        System.out.println("countPairsOther() iteration count: " + iterationCount);
         return pairs.size();
     }
 
@@ -73,12 +138,10 @@ class PairUtil {
             for (int b : numbers) {
                 iterationCount++;
                 if (a + k == b) {
-                    boolean added = pairs.add(new Pair(a, b));
-                    if (added) System.out.println(a + ", " + b + " pair added");
+                    pairs.add(new Pair(a, b));
                 }
             }
         }
-
         System.out.println("countPairsBrutForce() iteration count: " + iterationCount);
         return pairs.size();
     }
